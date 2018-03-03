78bd6a60a76a0e2058856120e287d040d23fda94
bananaprotocol <thebananaprotocol@gmail.com>
2018-03-03 23:52:17 +0100

    Update bot.js
    Remove the deprecated setGame function and replace it with the new setActivity one

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ let guilds = {};
 client.on("ready", function () {
   console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
   clientUser = client.user;
-  clientUser.setGame("some sick tunes!");
+  clientUser.setActivity("some sick Tunes!", {type: "PLAYING"});
 });
 
 client.on("message", function(message) {
